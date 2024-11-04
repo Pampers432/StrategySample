dac74052acdbb07752355254858aa627ffc7e4a1
Pampers432 <max23012007@gmail.com>
2024-11-04 14:11:24 +0300

    Add files via upload

---
+++ Strategy/Carry/CanCarry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Carry
+{
+    public class CanCarry : ICarry
+    {
+        public void Carry()
+        {
+            Console.WriteLine("I'm carry");
+        }
+    }
+}
+
