682e084ef3f70fa46a0e4f9cda50ae969a2e232b
Pampers432 <max23012007@gmail.com>
2024-11-04 14:10:44 +0300

    Add files via upload

---
+++ Strategy/Shoot/CanShoot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Shoot
+{
+    internal class CanShoot : IShoot
+    {
+        public void Shoot()
+        {
+            Console.WriteLine("I'm Shooting");
+        }
+    }
+}
+
---
+++ Strategy/Shoot/CantShoot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Shoot
+{
+    internal class CantShoot : IShoot
+    {
+        public void Shoot()
+        {
+            Console.WriteLine("I can't shoot");
+        }
+    }
+}
+
